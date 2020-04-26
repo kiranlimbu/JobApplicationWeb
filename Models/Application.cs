3f216e023bd5c1ed4b6ef55b64f44766a31f6b31
Kiran Limbu <58098835+kiranlimbu@users.noreply.github.com>
2020-04-26 15:55:52 -0700

    Add files via upload

---
+++ Models/Application.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+using VSLangProj;
+
+namespace JobApplicationPoster.Models
+{
+    public class Application
+    {
+        [Key]
+        public int Identy { get; set; }
+
+        [Display(Name = "Company Name")]
+        public string Company { get; set; }
+
+        [Display(Name = "Job Position")]
+        public string Title { get; set; }
+        public string Location { get; set; }
+        public int? Sticker { get; set; }
+
+        public int? StudentId { get; set; }
+        [ForeignKey("StudentIdentification")]
+
+        public virtual Student studentObj { get; set; }
+
+    }
+}
---
+++ Models/Student.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JobApplicationPoster.Models
+{
+    public class Student
+    {
+        [Key]
+        public int AutoId { get; set; }
+
+        [Display(Name = "Full Name")]
+        [Required]
+        public string FullName { get; set; }
+
+        public string Address { get; set; }
+
+        [Display(Name = "Current Title")]
+        public string Title { get; set; }
+        public int? Total { get; set; }
+        public virtual List<Application> applicationObj { get; set; }
+    }
+}
